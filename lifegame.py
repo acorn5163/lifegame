402d136f443d0074d70fce7f88dc4f7a1a839569
acorn5163 <70052650+acorn5163@users.noreply.github.com>
2021-01-09 16:23:49 +0900

    1.0
    四方向の判定、処理を実装

--- lifegame.py
+++ lifegame.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Dec 19 15:30:57 2020
-
 @author: keigo
 """
 
@@ -10,9 +9,10 @@ import random
 
 class LifeGame():
     def __init__(self):
-        self.size = 15
-        self.white = 30
+        self.size = 10
+        self.life = 50
         self.field = np.zeros((self.size,self.size), dtype = str)
+        self.fieldB = np.zeros((self.size,self.size), dtype = str)
         self.length = 0
         self.width = 0
         self.counter = 0
@@ -22,77 +22,122 @@ class LifeGame():
     def fieldmaker(self):
         for i in range(self.size):
             for j in range(self.size):
-                self.field[i,j] = '●'
-        for i in range(self.white):
-            self.field[random.randint(0,self.size-1),random.randint(0,self.size-1)] = '◯'
+                self.field[i,j] = '◯'
+        for i in range(self.life):
+            self.field[random.randint(0,self.size-1),random.randint(0,self.size-1)] = '●'
+        self.fieldB = self.field
+    
+        self.fieldB = self.field
     def printfield(self):
         print(self.field)
+        print("------------------")
     
     def checker(self):
-        if length == 0 and width == 0:
-            self.type = 5
-        elif length == 0 and width == size-1:
-            self.type = 6
-        elif length == size-1 and width == 0:
+        if self.length == 0 and self.width == 0:
             self.type = 7
-        elif length == size-1 and width == size-1:
+        elif self.length == 0 and self.width == self.size-1:
+            self.type = 5
+        elif self.length == self.size-1 and self.width == 0:
             self.type = 8
-        elif length == 0 and width != 0 and width != size-1:
+        elif self.length == self.size-1 and self.width == self.size-1:
+            self.type = 6
+        elif self.length == 0 and self.width != 0 and self.width != self.size-1:
             self.type = 1
-        elif length == size-1 and width != 0 and width != size-1:
+        elif self.length == self.size-1 and self.width != 0 and self.width != self.size-1:
             self.type = 2
-        elif width == size-1 and length != 0 and length != size-1:
+        elif self.width == self.size-1 and self.length != 0 and self.length != self.size-1:
             self.type = 3
-        elif width == 0 and length != 0 and length != size-1:
+        elif self.width == 0 and self.length != 0 and self.length != self.size-1:
             self.type = 4
         else:
             self.type = 0
 
+    
+    def Counter(self):
+        self.counter = 0
+        if self.type == 0:
+            if self.field[self.length+1,self.width] == '●':
+                self.counter += 1
+            if self.field[self.length-1,self.width] == '●':
+                self.counter += 1
+            if self.field[self.length,self.width+1] == '●':
+                self.counter += 1
+            if self.field[self.length,self.width-1] == '●':
+                self.counter += 1
             
+        if self.type == 1:
+            if self.field[self.length+1,self.width] == '●':
+                self.counter += 1
+            if self.field[self.length,self.width-1] == '●':
+                self.counter += 1
+            if self.field[self.length,self.width+1] == '●':
+                self.counter += 1
+        if self.type == 2:
+            if self.field[self.length-1,self.width] == '●':
+                self.counter += 1
+            if self.field[self.length,self.width-1] == '●':
+                self.counter += 1
+            if self.field[self.length,self.width+1] == '●':
+                self.counter += 1
+        if self.type == 3:
+            if self.field[self.length,self.width-1] == '●':
+                self.counter += 1
+            if self.field[self.length+1,self.width] == '●':
+                self.counter += 1
+            if self.field[self.length-1,self.width] == '●':
+                self.counter += 1
+        if self.type == 4:
+            if self.field[self.length,self.width+1] == '●':
+                self.counter += 1
+            if self.field[self.length+1,self.width] == '●':
+                self.counter += 1
+            if self.field[self.length-1,self.width] == '●':
+                self.counter += 1
+        if self.type == 5:
+            if self.field[self.length,self.width-1] == '●':
+                self.counter += 1
+            if self.field[self.length+1,self.width] == '●':
+                self.counter += 1
+        if self.type == 6:
+            if self.field[self.length-1,self.width] == '●':
+                self.counter += 1
+            if self.field[self.length,self.width-1] == '●':
+                self.counter += 1
+        if self.type == 7:
+            if self.field[self.length+1,self.width] == '●':
+                self.counter += 1
+            if self.field[self.length,self.width+1] == '●':
+                self.counter += 1
+        if self.type == 8:
+            if self.field[self.length-1,self.width] == '●':
+                self.counter += 1
+            if self.field[self.length,self.width+1] == '●':
+                self.counter += 1
     
-    def counting_four(self):
-        self.counter = 0
-        if self.field[self.length+1,self.width] = '◯':
-            self.counter += 1
-        if self.field[self.length-1,self.width] = '◯':
-            self.counter += 1
-        if self.field[self.length,self.width+1] = '◯':
-            self.counter += 1
-        if self.field[self.length,self.width-1] = '◯':
-            self.counter += 1
-    
-    def counting_three(self):
-        self.counter = 0
-        if self.type = 1:
-            if self.field[self.length+1,self.width] = '◯':
-            self.counter += 1
-            if self.field[self.length,self.width-1] = '◯':
-            self.counter += 1
-            if self.field[self.length,self.width+1] = '◯':
-            self.counter += 1
-        if self.type = 2:
-            if self.field[self.length-1,self.width] = '◯':
-            self.counter += 1
-            if self.field[self.length,self.width-1] = '◯':
-            self.counter += 1
-            if self.field[self.length,self.width+1] = '◯':
-            self.counter += 1
-        if self.type = 3:
-            if self.field[self.length,self.width-1] = '◯':
-            self.counter += 1
-            if self.field[self.length+1,self.width] = '◯':
-            self.counter += 1
-            if self.field[self.length-1,self.width] = '◯':
-            self.counter += 1
-        if self.type = 3:
-            if self.field[self.length,self.width+1] = '◯':
-            self.counter += 1
-            if self.field[self.length+1,self.width] = '◯':
-            self.counter += 1
-            if self.field[self.length-1,self.width] = '◯':
-            self.counter += 1
-    
-    def counting
+    def judger(self):
+        if self.field[self.length,self.width] == '●':
+            if self.counter == 0 or self.counter == 1 or self.counter == 4:
+                self.fieldB[self.length,self.width] = '●' 
+            else:
+                self.fieldB[self.length,self.width] = '◯'
+        
+        if self.field[self.length,self.width] == '◯':
+            if self.counter == 3:
+              self.fieldB[self.length,self.width] = '●' 
+            else:
+                self.fieldB[self.length,self.width] = '◯'
+
+    def mainsystem(self):
+        self.fieldmaker()
+        for i in range(10):
+            self.printfield()
+            for self.length in range(self.size):
+                for self.width in range(self.size):
+                    self.checker()
+                    self.Counter()
+                    self.judger()
+                    self.field = self.fieldB
+
+
 lifegame = LifeGame()
-lifegame.fieldmaker()
-lifegame.printfield()
+lifegame.mainsystem()
