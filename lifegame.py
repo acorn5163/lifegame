852e7d92f42789acbf3004640f32eab06251011a
acorn5163 <70052650+acorn5163@users.noreply.github.com>
2021-01-16 15:46:20 +0900

    2.1

--- lifegame.py
+++ lifegame.py
@@ -10,26 +10,34 @@ import random
 class LifeGame():
     def __init__(self):
         self.size = 10
-        self.life = 50
+        self.life = 14
         self.field = np.zeros((self.size,self.size), dtype = str)
         self.fieldB = np.zeros((self.size,self.size), dtype = str)
         self.length = 0
         self.width = 0
         self.counter = 0
         self.type = 0
+        self.custom = True
         
     
     def fieldmaker(self):
-        for i in range(self.size):
-            for j in range(self.size):
-                self.field[i,j] = '◯'
-        for i in range(self.life):
-            self.field[random.randint(0,self.size-1),random.randint(0,self.size-1)] = '●'
-        self.fieldB = self.field
+            for i in range(self.size):
+                for j in range(self.size):
+                    self.field[i,j] = '◯'
+            if self.custom == False:
+                for i in range(self.life):
+                    self.field[random.randint(0,self.size-1),random.randint(0,self.size-1)] = '●'
+            else:
+                self.field[1,4] = '●'
+                self.field[2,5] = '●'
+                self.field[3,5] = '●'
+                self.field[3,4] = '●'
+                self.field[3,3] = '●'
+            
+            self.fieldB = self.field
     
-        self.fieldB = self.field
     def printfield(self):
-        print(self.field)
+        print(self.fieldB)
         print("------------------")
     
     def checker(self):
@@ -56,21 +64,32 @@ class LifeGame():
     def Counter(self):
         self.counter = 0
         if self.type == 0:
-            if self.field[self.length+1,self.width] == '●':
+            if self.field[self.length-1,self.width] == '●': #上
                 self.counter += 1
-            if self.field[self.length-1,self.width] == '●':
+            if self.field[self.length+1,self.width] == '●': #下
                 self.counter += 1
-            if self.field[self.length,self.width+1] == '●':
+            if self.field[self.length,self.width+1] == '●': #右
                 self.counter += 1
-            if self.field[self.length,self.width-1] == '●':
+            if self.field[self.length,self.width-1] == '●': #左
                 self.counter += 1
-            
+            if self.field[self.length-1,self.width+1] == '●': #右上
+                self.counter += 1
+            if self.field[self.length-1,self.width-1] == '●': #左上
+                self.counter += 1
+            if self.field[self.length+1,self.width+1] == '●': #右下
+                self.counter += 1
+            if self.field[self.length+1,self.width-1] == '●': #左下
+                self.counter += 1
         if self.type == 1:
             if self.field[self.length+1,self.width] == '●':
                 self.counter += 1
             if self.field[self.length,self.width-1] == '●':
                 self.counter += 1
             if self.field[self.length,self.width+1] == '●':
+                self.counter += 1
+            if self.field[self.length+1,self.width+1] == '●': #右下
+                self.counter += 1
+            if self.field[self.length+1,self.width-1] == '●': #左下
                 self.counter += 1
         if self.type == 2:
             if self.field[self.length-1,self.width] == '●':
@@ -79,12 +98,21 @@ class LifeGame():
                 self.counter += 1
             if self.field[self.length,self.width+1] == '●':
                 self.counter += 1
+            if self.field[self.length-1,self.width+1] == '●': #右上
+                self.counter += 1
+            if self.field[self.length-1,self.width-1] == '●': #左上
+                self.counter += 1
+            
         if self.type == 3:
             if self.field[self.length,self.width-1] == '●':
                 self.counter += 1
             if self.field[self.length+1,self.width] == '●':
                 self.counter += 1
             if self.field[self.length-1,self.width] == '●':
+                self.counter += 1
+            if self.field[self.length-1,self.width-1] == '●': #左上
+                self.counter += 1
+            if self.field[self.length+1,self.width-1] == '●': #左下
                 self.counter += 1
         if self.type == 4:
             if self.field[self.length,self.width+1] == '●':
@@ -93,30 +121,44 @@ class LifeGame():
                 self.counter += 1
             if self.field[self.length-1,self.width] == '●':
                 self.counter += 1
+            if self.field[self.length-1,self.width+1] == '●': #右上
+                self.counter += 1
+            if self.field[self.length+1,self.width+1] == '●': #右下
+                self.counter += 1
         if self.type == 5:
             if self.field[self.length,self.width-1] == '●':
                 self.counter += 1
             if self.field[self.length+1,self.width] == '●':
+                self.counter += 1
+            if self.field[self.length+1,self.width-1] == '●': #左下
                 self.counter += 1
         if self.type == 6:
             if self.field[self.length-1,self.width] == '●':
                 self.counter += 1
             if self.field[self.length,self.width-1] == '●':
                 self.counter += 1
+            if self.field[self.length-1,self.width-1] == '●': #左上
+                self.counter += 1
         if self.type == 7:
             if self.field[self.length+1,self.width] == '●':
                 self.counter += 1
             if self.field[self.length,self.width+1] == '●':
+                self.counter += 1
+            if self.field[self.length+1,self.width+1] == '●': #右下
                 self.counter += 1
         if self.type == 8:
             if self.field[self.length-1,self.width] == '●':
                 self.counter += 1
             if self.field[self.length,self.width+1] == '●':
                 self.counter += 1
+            if self.field[self.length-1,self.width+1] == '●': #右上
+                self.counter += 1
+            
     
     def judger(self):
+        print("Length = " + str(self.length)+ "|" + "width = " + str(self.width)+ "|" + "count = " + str(self.counter)+ "|" + "type = " + str(self.type))
         if self.field[self.length,self.width] == '●':
-            if self.counter == 0 or self.counter == 1 or self.counter == 4:
+            if self.counter == 2 or self.counter == 3:
                 self.fieldB[self.length,self.width] = '●' 
             else:
                 self.fieldB[self.length,self.width] = '◯'
@@ -129,7 +171,7 @@ class LifeGame():
 
     def mainsystem(self):
         self.fieldmaker()
-        for i in range(10):
+        for i in range(3):
             self.printfield()
             for self.length in range(self.size):
                 for self.width in range(self.size):
